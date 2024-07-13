3f51d645fe754ba9750787b1bc0803bc3b3a766c
Sidrrish <159316987+Sidrrish@users.noreply.github.com>
2024-07-13 11:07:02 +0530

    Add files via upload

---
+++ main_screen.py
@@ -0,0 +1,45 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+from time import sleep
+
+screen = Screen()
+screen.setup(height=600, width=800)
+screen.bgcolor("black")
+screen.title("My Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.move_up, "Up")
+screen.onkey(r_paddle.move_down, "Down")
+screen.onkey(l_paddle.move_up, "w")
+screen.onkey(l_paddle.move_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    sleep(ball.move_speed)
+    ball.move()
+
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.r_scores()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.l_scores()
+
+screen.exitonclick()
